08502e4f9654a1e268c35ac4ab882427018f6116
Sahal Palayat <sahalpalayat5894@gmail.com>
2024-03-06 09:41:57 +0530

    ll

--- react-ui/src/redux.jsx
+++ react-ui/src/redux.jsx
@@ -54,7 +54,6 @@ export const reducer = (state = initialState, action) => {
                     gender: action.payload.gender
                 }
             };
-
         default:
             return state;
     }
